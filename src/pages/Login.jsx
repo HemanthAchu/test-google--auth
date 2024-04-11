4df8aedf93e04f227e9c8971c6ade4904975d1af
HemanthAchu <hemanthms186@gmail.com>
2024-04-11 08:32:35 +0530

    Google-Auth

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from 'jwt-decode';
 
- import jwtDecode from 'jwt-decode'
 
 function Login({setemail}) {
   return (
